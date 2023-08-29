862ce177b63139eba3eae9d1ba6289840aa67fea
AndroidHQ254 <d.murimi14@gmail.com>
2023-08-29 04:25:22 +0300

    misound: Adapt for toco

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -9,7 +9,7 @@
 set -e
 
 DEVICE=common
-VENDOR=xiaomi/sweet-misound
+VENDOR=xiaomi/toco-misound
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -28,12 +28,12 @@ source "${HELPER}"
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "sweet"
-sed -i 's|vendor/xiaomi/sweet-misound/|vendor/xiaomi/sweet-misound/common|g' "${PRODUCTMK}"
-sed -i 's|device/xiaomi/sweet-misound//setup-makefiles.sh|vendor/xiaomi/sweet-misound/setup-makefiles.sh|g' "${ANDROIDBP}" "${ANDROIDMK}" "${BOARDMK}" "${PRODUCTMK}"
+write_headers "toco"
+sed -i 's|vendor/xiaomi/toco-misound/|vendor/xiaomi/toco-misound/common|g' "${PRODUCTMK}"
+sed -i 's|device/xiaomi/toco-misound//setup-makefiles.sh|vendor/xiaomi/toco-misound/setup-makefiles.sh|g' "${ANDROIDBP}" "${ANDROIDMK}" "${BOARDMK}" "${PRODUCTMK}"
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
-sed -i 's|"xiaomi/sweet-misound"|"xiaomi"|g' "${ANDROIDBP}"
+sed -i 's|"xiaomi/toco-misound"|"xiaomi"|g' "${ANDROIDBP}"
 
 # Finish
 write_footers
